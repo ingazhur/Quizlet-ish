cd7da6355704bcd86e5971eb342ad29a45af2f0c
ingazhur <ingazhuravlev@gmail.com>
2022-11-26 12:09:59 -0800

    Initial Commit

---
+++ Quizlet-ish/Quizlet_ishApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quizlet_ishApp.swift
+//  Quizlet-ish
+//
+//  Created by Inga on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Quizlet_ishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
